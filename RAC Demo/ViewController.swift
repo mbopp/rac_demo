cbc50be9728ea1984be3816ef6bc97fa3497b9d7
Michael Bopp <mbopp@rapiddg.com>
2016-05-01 15:22:59 -0400

    Initial Commit

---
+++ RAC Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RAC Demo
+//
+//  Created by Michael Bopp on 5/1/16.
+//  Copyright © 2016 Michael Bopp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
